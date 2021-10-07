cb186aab6c6bf45c451a5ec50b90410e5cf2de63
monte-flora <monte.flora@noaa.gov>
2021-10-07 16:28:16 -0500

    updating version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ URL = "https://github.com/monte-flora/py-mint/"
 EMAIL = "monte.flora@noaa.gov"
 AUTHOR = "Montgomery Flora"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.2.2"
+VERSION = "0.2.4"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
