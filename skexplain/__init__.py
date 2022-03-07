69117e2f629c17a7de7ff56d5e57bf2e8e853df8
monte-flora <monte.flora@noaa.gov>
2022-03-07 09:00:18 -0600

    updating the __init__ file

--- skexplain/__init__.py
+++ skexplain/__init__.py
@@ -1,13 +1,13 @@
 
 # Import the main package
-from .main.interpret_toolkit import InterpretToolkit
-from .main.global_interpret import GlobalInterpret
-from .main.local_interpret import LocalInterpret
+from .main.explain_toolkit import ExplainToolkit
+from .main.global_explainer import GlobalExplainer
+from .main.local_explainer import LocalExplainer
 
 # Import data for notebooks
 from .common.models import load_models
 from .common.dataset import load_data
 
-__version__ = '0.2.6' 
+__version__ = '0.0.1' 
 
 
